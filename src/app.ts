f71338aced1a5832335c6bb29e57aa88fc86f83b
giout <giovanniurdanetatulli@gmail.com>
2024-08-19 23:37:59 -0400

    build: project initialization
    - initialize app object
    - define folder structure

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+
+export default app
