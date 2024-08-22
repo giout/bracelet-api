acea03b9b4953169b36a125b7490c5692dec0df3
giout <giovanniurdanetatulli@gmail.com>
2024-08-22 10:07:55 -0400

    feat: retrieve username and id_card at get /assignment

--- db/queries/assignment_history/get-all.sql
+++ db/queries/assignment_history/get-all.sql
@@ -1,6 +1,12 @@
 SELECT 
-    *
+    a.*,
+    u.username,
+    u.id_card
 FROM 
     assignment_history a
+INNER JOIN 
+    "user" u
+ON 
+    a."user"=a."user"
 LIMIT $(limit)::integer
 OFFSET ($(limit)::integer*$(page)::integer) - $(limit)::integer;
